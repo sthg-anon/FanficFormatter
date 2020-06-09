9da55453ac565d8ab01c94504fec161835f9b4cd
sthg anon <sthg.anon@gmail.com>
2020-06-08 17:01:52 -0700

    Initial Commit

---
+++ FanficFormatter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FanficFormatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
