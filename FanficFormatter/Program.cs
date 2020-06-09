7d27bfcba68ba0411d2e2fcdeb2c395221db50ed
sthg anon <sthg.anon@gmail.com>
2020-06-08 17:23:36 -0700

    Some housekeeping with stylecop

--- FanficFormatter/Program.cs
+++ FanficFormatter/Program.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿// DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+// Version 2, December 2004
+//
+// Copyright (C) 2020 sthg anon (sthg.anon@gmail.com)
+//
+// Everyone is permitted to copy and distribute verbatim or modified
+// copies of this license document, and changing it is allowed as long
+// as the name is changed.
+//
+// DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+// TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//
+// 0. You just DO WHAT THE FUCK YOU WANT TO.
 
 namespace FanficFormatter
 {
-    class Program
+    using System;
+
+    /// <summary>
+    ///     Contains the main program entry point.
+    /// </summary>
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
